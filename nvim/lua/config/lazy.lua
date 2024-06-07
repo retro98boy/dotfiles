c930718780aeea4bf78f3cbf1052c5d67f79c5f5
retro98boy <retro98boy@qq.com>
2024-06-07 09:39:03 +0800

    nvim: fix the import path of mini-surround

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,7 +26,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.editor.outline" },
-    { import = "lazyvim.plugins.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.lang.clangd" },

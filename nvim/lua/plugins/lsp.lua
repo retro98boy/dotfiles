86ddf01a2475132cb397ffe73145fdc495806cd2
retro98boy <retro98boy@qq.com>
2024-06-12 14:52:41 +0800

    nvim: format all

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,8 +7,13 @@ return {
       keys[#keys + 1] = { "gK", false }
       keys[#keys + 1] = { "<C-k>", false }
       keys[#keys + 1] = { "<F12>", vim.lsp.buf.hover, mode = { "n", "i" }, desc = "Hover" }
-      keys[#keys + 1] =
-        { "<F11>", vim.lsp.buf.signature_help, mode = { "n", "i" }, desc = "Signature Help", has = "signatureHelp" }
+      keys[#keys + 1] = {
+        "<F11>",
+        vim.lsp.buf.signature_help,
+        mode = { "n", "i" },
+        desc = "Signature Help",
+        has = "signatureHelp",
+      }
     end,
   },
 }

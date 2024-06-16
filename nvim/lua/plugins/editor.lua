4cf165c049af931992dc34701952a9a7db9d8269
retro98boy <retro98boy@qq.com>
2024-06-16 16:11:48 +0800

    nvim: update keymaps

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,6 +1,9 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>ef", "<cmd>Neotree focus<cr>", desc = "Neotree focus" },
+    },
     opts = {
       window = {
         mappings = {

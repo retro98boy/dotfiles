afee3204cb5a71a10dda4072bf9df33b64aa3ffd
retro98boy <retro98boy@qq.com>
2024-07-07 17:44:30 +0800

    nvim: update lazyvim starter

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,21 +1,19 @@
 -- 自举
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Lazy.nvim界面快捷键
-require("lazy.view.config").keys = {
-  hover = "<F12>",
-  diff = "d",
-  close = "q",
-  details = "<cr>",
-  profile_sort = "<C-s>",
-  profile_filter = "<C-f>",
-  abort = "<C-c>",
-}
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "

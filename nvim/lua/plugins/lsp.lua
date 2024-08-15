eee4ccfd0e4d2c5925803d0c6dfc770ac4768739
retro98boy <retro98boy@qq.com>
2024-08-15 13:20:51 +0800

    nvim: fix some keymaps occasionally failing

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,8 +7,11 @@ return {
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = { "K", "10k" }
       keys[#keys + 1] = { "gK", false }
       keys[#keys + 1] = { "<C-k>", false }
+      keys[#keys + 1] = { "<C-k>", "<Up>", mode = { "i" } }
+      keys[#keys + 1] = { "<C-k>", "<C-w>k", mode = { "n" } }
       keys[#keys + 1] = { "<F12>", vim.lsp.buf.hover, mode = { "n", "i" }, desc = "Hover" }
       keys[#keys + 1] = {
         "<F11>",
